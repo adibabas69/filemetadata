e82752d27ba6cf5ce026b37bdf6ea7033bf02847
Glitch (wandering-court) <none>
2018-04-02 11:35:52 +0800

    first commit

--- server.js
+++ server.js
@@ -2,14 +2,29 @@
 // where your node app starts
 
 // init project
-const express = require('express')
-const app = express()
+var bodyParser =  require('body-parser')
+var cors = require('cors')
+var multer = require('multer')
+var express = require('express')
+var upload = multer({ dest: 'uploads/' })
+var app = module.exports = express()
+app.use(bodyParser.json());
+app.use(cors())
+
+
+// app.get('/',function(req,res,next){
+// res.sendFile(__dirname + "/views/index.html");
+// });
+
+app.post('/upload',upload.single('file'),function (req,res,next){
+return res.json(req.file);
+});
 
 // we've started you off with Express, 
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'))
+app.use(express.static(__dirname + 'public'))
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", (request, response) => {
@@ -17,21 +32,7 @@ app.get("/", (request, response) => {
 })
 
 // Simple in-memory store
-const dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-]
 
-app.get("/dreams", (request, response) => {
-  response.send(dreams)
-})
-
-// could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
-app.post("/dreams", (request, response) => {
-  dreams.push(request.query.dream)
-  response.sendStatus(200)
-})
 
 // listen for requests :)
 const listener = app.listen(process.env.PORT, () => {
